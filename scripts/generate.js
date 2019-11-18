6594a6cc0d602d2134e6cc19902791e5b8e6c881
roll <eskarev@gmail.com>
2019-11-18 11:07:51 +0300

    Normalized data

--- scripts/generate.js
+++ scripts/generate.js
@@ -3,18 +3,35 @@ const faker = require('faker')
 const moment = require('moment')
 const csv = require('papaparse')
 
-for (const index of Array(10).keys()) {
-  const rows = 1500000
-  const csvStream = fs.createWriteStream('data/data1000.csv', {flags: 'a'})
-  for (const rowNumber of Array(rows).keys()) {
-    const string = faker.name.findName()
-    const integer = faker.random.number(rows)
-    const number = parseFloat(faker.finance.amount(0, 1000, 3))
-    const booleans = faker.random.boolean()
-    const datetime = moment(faker.date.past(100)).format('YYYY-MM-DDThh:mm:ss') + 'Z'
-    const date = moment(faker.date.past(100)).format('YYYY-MM-DD')
-    const time = moment(faker.date.past(100)).format('hh:mm:ss')
-    const row = [string, integer, number, booleans, datetime, date, time]
-    csvStream.write(csv.unparse([row]) + "\n")
+const tasks = 10
+const mBytes = process.argv[2]
+
+const main = async () => {
+  for (const index of Array(tasks).keys()) {
+    const options = index ? {flags: 'a'} : {}
+    await write(options)
   }
 }
+
+const write = async (options) => {
+  return new Promise((resolve, reject) => {
+    const rows = mBytes * 1000000 / tasks / 100
+    const csvStream = fs.createWriteStream(`data/data${mBytes}.csv`, options)
+    for (const rowNumber of Array(rows).keys()) {
+      const string = faker.random.alphaNumeric(40)
+      const integer = faker.random.number({min: 10000, max: 99999})
+      const number = parseFloat(faker.finance.amount(10000, 99999, 3)).toFixed(3)
+      const bool = faker.random.boolean() ? '1' : '0'
+      const datetime = moment(faker.date.past(100)).format('YYYY-MM-DDThh:mm:ss') + 'Z'
+      const date = moment(faker.date.past(100)).format('YYYY-MM-DD')
+      const time = moment(faker.date.past(100)).format('hh:mm:ss')
+      const row = [string, integer, number, bool, datetime, date, time]
+      csvStream.write(row.join(',') + "\n")
+    }
+    csvStream.end(resolve)
+  })
+}
+
+main()
+  .then(result => {})
+  .catch(error => console.log(error))
