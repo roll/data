e65602818c25b5872da4696332fdadfd6efd8973
roll <eskarev@gmail.com>
2019-12-18 10:19:35 +0300

    Added data5, data50

--- scripts/generate.js
+++ scripts/generate.js
@@ -4,7 +4,7 @@ const moment = require('moment')
 const csv = require('papaparse')
 
 const tasks = 10
-const mBytes = process.argv[2]
+const mBytes = parseInt(process.argv[2])
 
 const main = async () => {
   for (const index of Array(tasks).keys()) {
@@ -15,10 +15,12 @@ const main = async () => {
 
 const write = async (options) => {
   return new Promise((resolve, reject) => {
-    const rows = mBytes * 1000000 / tasks / 100
+    const isFull = [1, 10, 100, 1000].includes(mBytes)
+    const rows = mBytes * 1000000 / tasks / (isFull ? 100 : 50)
     const csvStream = fs.createWriteStream(`data/data${mBytes}.csv`, options)
     for (const rowNumber of Array(rows).keys()) {
-      const string = faker.random.alphaNumeric(7)
+      const string1 = faker.random.alphaNumeric(7)
+      const string2 = faker.random.alphaNumeric(11)
       const integer = faker.random.number({min: 100000, max: 999999})
       const decimal = parseFloat(faker.finance.amount(100, 999, 3)).toFixed(3)
       const flt = parseFloat(faker.finance.amount(100, 999, 3)).toFixed(3)
@@ -28,7 +30,9 @@ const write = async (options) => {
       const time = moment(faker.date.past(100)).format('hh:mm:ss')
       const array = [faker.random.number({min: 1, max: 9}), faker.random.number({min: 1, max: 9})].join(',')
       const object = JSON.stringify({x: faker.random.number({min: 1, max: 9}), y: faker.random.number({min: 1, max: 9})})
-      const row = [string, integer, decimal, flt, bool, datetime, date, time, array, object]
+      const row = isFull
+        ? [string1, integer, decimal, flt, bool, datetime, date, time, array, object]
+        : [string2, integer, flt, bool, datetime]
       csvStream.write(csv.unparse([row]) + "\n")
     }
     csvStream.end(resolve)
