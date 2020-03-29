c8719d4eaa1a86eee6bd99d3fa59c4275c1e234e
roll <eskarev@gmail.com>
2020-03-29 11:41:51 +0300

    Rebased on true/false for boolean

--- scripts/generate.js
+++ scripts/generate.js
@@ -8,37 +8,44 @@ const mBytes = parseInt(process.argv[2])
 
 const main = async () => {
   for (const index of Array(tasks).keys()) {
-    const options = index ? {flags: 'a'} : {}
+    const options = index ? { flags: 'a' } : {}
     await write(options)
   }
 }
 
 const write = async (options) => {
   return new Promise((resolve, reject) => {
-    const isFull = [1, 10, 100, 1000].includes(mBytes)
-    const rows = mBytes * 1000000 / tasks / (isFull ? 100 : 50)
+    const isL = [1, 10, 100, 1000].includes(mBytes)
+    const rows = (mBytes * 1000000) / tasks / (isL ? 100 : 50)
     const csvStream = fs.createWriteStream(`data/data${mBytes}.csv`, options)
     for (const rowNumber of Array(rows).keys()) {
-      const string1 = faker.random.alphaNumeric(7)
-      const string2 = faker.random.alphaNumeric(11)
-      const integer = faker.random.number({min: 100000, max: 999999})
-      const decimal = parseFloat(faker.finance.amount(100, 999, 3)).toFixed(3)
-      const flt = parseFloat(faker.finance.amount(100, 999, 3)).toFixed(3)
-      const bool = faker.random.boolean() ? '1' : '0'
+      const isT = faker.random.boolean()
+      const string1 = faker.random.alphaNumeric(6)
+      const string2 = faker.random.alphaNumeric(9)
+      const integer = faker.random.number({ min: 10000, max: 99999 })
+      const decimal = parseFloat(faker.finance.amount(100, 999, 2)).toFixed(2)
+      const float = parseFloat(faker.finance.amount(100, 999, isT ? 3 : 2)).toFixed(isT ? 3 : 2)
+      const bool = isT ? 'true' : 'false'
       const datetime = moment(faker.date.past(100)).format('YYYY-MM-DDThh:mm:ss') + 'Z'
       const date = moment(faker.date.past(100)).format('YYYY-MM-DD')
       const time = moment(faker.date.past(100)).format('hh:mm:ss')
-      const array = [faker.random.number({min: 1, max: 9}), faker.random.number({min: 1, max: 9})].join(',')
-      const object = JSON.stringify({x: faker.random.number({min: 1, max: 9}), y: faker.random.number({min: 1, max: 9})})
-      const row = isFull
-        ? [string1, integer, decimal, flt, bool, datetime, date, time, array, object]
-        : [string2, integer, flt, bool, datetime]
-      csvStream.write(csv.unparse([row]) + "\n")
+      const array = [
+        faker.random.number({ min: 1, max: 9 }),
+        faker.random.number({ min: 1, max: 9 }),
+      ].join(',')
+      const object = JSON.stringify({
+        x: faker.random.number({ min: 1, max: 9 }),
+        y: faker.random.number({ min: 1, max: 9 }),
+      })
+      const row = isL
+        ? [string1, integer, decimal, float, bool, datetime, date, time, array, object]
+        : [string2, integer, float, bool, datetime]
+      csvStream.write(csv.unparse([row]).replace('" true"', ' true') + '\n')
     }
     csvStream.end(resolve)
   })
 }
 
 main()
-  .then(result => {})
-  .catch(error => console.log(error))
+  .then((result) => {})
+  .catch((error) => console.log(error))
